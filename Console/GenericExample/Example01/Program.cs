51c3cbfd66a1a21aa98a8b9d5f3c69edf239958b
Mehnaz Tabassum <mtabassum015@gmail.com>
2020-11-15 18:51:18 +0600

    commit examples of Generics

---
+++ Console/GenericExample/Example01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Example01
+{
+     class GenericExample<T>
+        {
+            private T genericVariable;
+
+            public GenericExample(T genericValue)
+            {
+                this.genericVariable = genericValue;
+            }
+
+            public void Display()
+            {
+                Console.WriteLine(this.genericVariable);
+            }
+        }
+
+        class Test
+        {
+            static void Main(string[] args)
+            {
+                GenericExample<int> g = new GenericExample<int>(5);
+
+                GenericExample<string> g1 = new GenericExample<string>("Example of Generic");
+
+                g.Display();
+                g1.Display();
+            }
+        }
+    }
---
+++ Console/GenericExample/Example02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Example02
+{
+    class Program
+    {
+		static List<T> GetList<T>(T value, int count)
+        {
+            List<T> list = new List<T>();
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(value);
+            }
+            return list;
+        }
+
+        static void Main()
+        {
+            List<bool> list1 = GetList(true, 3);
+            List<string> list2 = GetList<string>("Paris", 2);
+            foreach (bool value in list1)
+            {
+                Console.WriteLine(value);
+            }
+            foreach (string value in list2)
+            {
+                Console.WriteLine(value);
+            }
+        }
+	}
+}
