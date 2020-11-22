a311c53b85fb9c159797c6c82666012916dd1458
Mehnaz Tabassum <mtabassum015@gmail.com>
2020-11-23 00:48:29 +0600

    Added 2 solutions

---
+++ Console/Codeforces-Solution/CF_1220A_Cards/Program.cs
@@ -0,0 +1,32 @@
+﻿// http://codeforces.com/problemset/problem/1220/A
+
+
+using System;
+
+namespace CF_1220A_Cards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int one = 0, zero = 0;
+             string str = string.Empty;
+
+            var length = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < length; i++)
+            {
+                if (str[i] == 'n')
+                    Console.WriteLine("1 ");
+                else if (str[i] == 'z')
+                    zero++;
+            }
+
+
+            for(int i=0; i<zero; i++)
+            {
+                Console.WriteLine("0 ");
+            }
+        }
+    }
+}
---
+++ Console/Codeforces-Solution/CF_791A_Bear_BigBrother/Program.cs
@@ -0,0 +1,29 @@
+﻿// https://codeforces.com/contest/791/problem/A
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CF_791A_Bear_BigBrother
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var input = Console.ReadLine().Split(' ');
+
+            var lW = int.Parse(input[0]);
+            var bW = int.Parse(input[1]);
+            var count = 0;
+
+            while (lW <= bW)
+            {
+                lW = lW * 3;
+                bW = bW * 2;
+                count++;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
