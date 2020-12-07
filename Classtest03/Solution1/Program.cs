0d7bdf0d72e78e3131f89e2d08045b0fb425c34b
Mehnaz Tabassum <mtabassum015@gmail.com>
2020-12-07 22:20:57 +0600

    commit solution of question no 1

---
+++ Classtest03/Solution1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Solution1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int co1, co2;
+
+            Console.Write("Find the quadrant in which the coordinate point lies:\n");
+
+            Console.Write("Input the value for X coordinate :");
+            co1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the value for Y coordinate :");
+            co2 = Convert.ToInt32(Console.ReadLine());
+
+
+            if (co1 > 0 && co2 > 0)
+                Console.Write("The coordinate point ({0} {1}) lies in the First quadrant.\n\n", co1, co2);
+            else if (co1 < 0 && co2 > 0)
+                Console.Write("The coordinate point ({0} {1}) lies in the Second quadrant.\n\n", co1, co2);
+            else if (co1 < 0 && co2 < 0)
+                Console.Write("The coordinate point ({0} {1}) lies in the Third quadrant.\n\n", co1, co2);
+            else if (co1 > 0 && co2 < 0)
+                Console.Write("The coordinate point ({0} {1}) lies in the Fourth quadrant.\n\n", co1, co2);
+            else if (co1 == 0 && co2 == 0)
+                Console.Write("The coordinate point ({0} {1}) lies at the origin.\n\n", co1, co2);
+        }
+    }
+}
