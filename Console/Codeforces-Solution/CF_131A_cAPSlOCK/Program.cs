c978b0779a5dcd8878f24613f79739ae76c586fe
Mehnaz Tabassum <mtabassum015@gmail.com>
2020-12-16 18:58:19 +0600

    commit a few solution

---
+++ Console/Codeforces-Solution/CF_131A_cAPSlOCK/Program.cs
@@ -0,0 +1,37 @@
+﻿// https://codeforces.com/problemset/problem/131/A
+
+
+using System;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace CF_131A_cAPSlOCK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allLetterCapital = true;
+            char  u ;
+            var word = Console.ReadLine();
+
+            for (int i = 1; i < word.Length; i++)
+            {
+                if (char.IsLower(word[i]))
+                    allLetterCapital = false;
+            }
+
+            if (allLetterCapital)
+            {
+                for (int j = 0; j < word.Length; j++)
+                {
+                    if (char.IsLower(word[j]))
+                       char.ToUpper(word[j]);
+
+                    Console.WriteLine(word);
+                }
+            }
+
+            
+        }
+    }
+}
---
+++ Console/Codeforces-Solution/CF_510A_Fox-Snake/Program.cs
@@ -0,0 +1,26 @@
+﻿// https://codeforces.com/problemset/problem/510/A
+
+
+using System;
+
+namespace CF_510A_Fox_Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ');
+
+            var row = int.Parse(input[0]);
+            var column = int.Parse(input[1]);
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    
+                }
+            }
+        }
+    }
+}
